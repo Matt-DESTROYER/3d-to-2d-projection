311cb81321ba9fe023aac2dbd0d9eb1853b594fb
Matthew James <matthewsalgorithm@gmail.com>
2026-04-08 16:38:57 +1000

    Cleanup initialisation and actually initialise objects

--- src/scripts/main.mjs
+++ src/scripts/main.mjs
@@ -1,10 +1,4 @@
-import { Matrix } from "./matrix.mjs";
 import { GameObject } from "./game-object.mjs"
-
-const canvas = document.createElement("canvas");
-const ctx = canvas.getContext("2d");
-
-const camera = new Matrix(3, 1);
 
 function resize() {
 	canvas.width = window.innerWidth;
@@ -17,19 +11,54 @@ function render() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function game_loop() {
+function game_loop(ctx, camera, gameobjects) {
 	//update();
 	render();
 
-	window.requestAnimationFrame(game_loop);
+	window.requestAnimationFrame(() => game_loop(ctx, camera, gameobjects));
 }
 
-function main() {
+async function main() {
+	const canvas = document.createElement("canvas");
+
 	resize();
 	window.addEventListener("resize", resize);
 	document.body.appendChild(canvas);
 
-	window.requestAnimationFrame(game_loop);
+	const ctx = canvas.getContext("2d");
+	
+	const camera = new GameObject(new Point(0, 0, 0), [], []);
+	const gameobjects = [];
+	
+	const cube = new GameObject(new Point(0, 0, 5), [
+		new Point(-1, -1, -1),
+		new Point(-1, -1, 1),
+		new Point(1, -1, 1),
+		new Point(1, -1, -1),
+		new Point(-1, 1, -1),
+		new Point(-1, 1, 1),
+		new Point(1, 1, 1),
+		new Point(1, 1, -1)
+	], [
+		// top square
+		[1, 2],
+		[2, 3],
+		[3, 4],
+		[4, 1],
+		// bottom square
+		[5, 6],
+		[6, 7],
+		[7, 8],
+		[8, 5],
+		// connections
+		[1, 5],
+		[2, 6],
+		[3, 7],
+		[4, 8]
+	]);
+	gameobjects.push(cube);
+
+	window.requestAnimationFrame(() => game_loop(ctx, camera, gameobjects));
 }
 
 if (document.readyState === "loading") {
