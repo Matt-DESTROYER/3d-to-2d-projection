4f84d71f55744254ec4c1a8e600f161fa55c5cca
Matthew James <matthewsalgorithm@gmail.com>
2026-04-08 18:19:02 +1000

    Camera movement mixup fix

--- src/scripts/main.mjs
+++ src/scripts/main.mjs
@@ -35,16 +35,16 @@ class Game {
 
 	update(delta_time) {
 		if (this.input.forward) {
-			this.camera.z(this.camera.x() + CAM_SPEED * delta_time);
+			this.camera.z(this.camera.z() + CAM_SPEED * delta_time);
 		}
 		if (this.input.backward) {
-			this.camera.z(this.camera.x() - CAM_SPEED * delta_time);
+			this.camera.z(this.camera.z() - CAM_SPEED * delta_time);
 		}
 		if (this.input.left) {
-			this.camera.x(this.camera.z() + CAM_SPEED * delta_time);
+			this.camera.x(this.camera.x() + CAM_SPEED * delta_time);
 		}
 		if (this.input.right) {
-			this.camera.x(this.camera.z() - CAM_SPEED * delta_time);
+			this.camera.x(this.camera.x() - CAM_SPEED * delta_time);
 		}
 		if (this.input.up) {
 			this.camera.y(this.camera.y() + CAM_SPEED * delta_time);
