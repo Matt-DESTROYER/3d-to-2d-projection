745bfb238be09e291f786a3a8afd2ecb6459d45f
Matthew James <matthewsalgorithm@gmail.com>
2026-04-08 20:12:03 +1000

    Fixed parsing logic

--- src/scripts/wavefront-object.mjs
+++ src/scripts/wavefront-object.mjs
@@ -14,24 +14,28 @@ export function wavefront_to_gameobject(data) {
 			continue;
 
 		if (parts[0] === "v") {
-			let x = Number(lines[1]);
-			let y = Number(lines[2]);
-			let z = Number(lines[3]);
+			let x = Number(parts[1]);
+			let y = Number(parts[2]);
+			let z = Number(parts[3]);
 
 			vertices.push(new Point(x, y, z));
 		} else if (parts[0] === "f" || parts[0] === "l") {
 			// check for vertex normal indices ('/')
 			// or vertex normal indicies without texture coordinate indices ("//")
-			if (parts[1].includes("/"))
-				continue;
+			for (let i = 1; i < parts.length; i++) {
+				if (parts[i].includes("/")) {
+					const sub_parts = parts[i].split(/\/+/);
+					parts[i] = sub_parts[0];
+				}
+			}
 			
 			for (let i = 1; i < parts.length - 1; i++) {
 				edges.push([
-					Number(parts[i]),
-					Number(parts[i + 1])
+					Number(parts[i]) - 1,
+					Number(parts[i + 1]) - 1
 				]);
 			}
-			edges.push([ Number(parts[1]), Number(parts[parts.length - 1]) ])
+			edges.push([ Number(parts[1]) - 1, Number(parts[parts.length - 1]) - 1 ])
 		}
 	}
 
